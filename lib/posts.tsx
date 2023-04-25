7fabda7af4995d61d0166dd8b2f4203075f4bac6
jamiekim828 <jamiekim828@gmail.com>
2023-04-26 01:04:37 +0200

    blog posts render

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import { remark } from 'remark';
+import html from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), 'blogposts');
 
@@ -24,4 +26,26 @@ export default function getSortedPostsData() {
     return blogPost;
   });
   return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  const matterResult = matter(fileContents);
+
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+
+  const contentHtml = processedContent.toString();
+
+  const blogPostWithHTML: Blogpost & { contentHtml: string } = {
+    id,
+    title: matterResult.data.title,
+    date: matterResult.data.date,
+    contentHtml,
+  };
+
+  return blogPostWithHTML;
 }
